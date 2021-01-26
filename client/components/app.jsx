e45b39a21b2333726bd6497b963cebfa0cf1b8ed
Remy Orans <remy.orans@gmail.com>
2021-01-25 19:38:34 -0800

    Fix get url, remove top bottom dummy containers and global style

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -8,54 +8,6 @@ import NavBar from './navBar.jsx'
 import axios from 'axios';
 import moment from 'moment';
 
-const GlobalStyle = createGlobalStyle`
-  html, body, div, span, applet, object, iframe,
-  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-  a, abbr, acronym, address, big, cite, code,
-  del, dfn, em, img, ins, kbd, q, s, samp,
-  small, strike, strong, sub, sup, tt, var,
-  b, u, i, center,
-  dl, dt, dd, ol, ul, li,
-  fieldset, form, label, legend,
-  table, caption, tbody, tfoot, thead, tr, th, td,
-  article, aside, canvas, details, embed,
-  figure, figcaption, footer, header, hgroup,
-  menu, nav, output, ruby, section, summary,
-  time, mark, audio, video {
-    margin: 0;
-    padding: 0;
-    border: 0;
-    font-size: 100%;
-    font: inherit;
-    vertical-align: baseline;
-  }
-
-  article, aside, details, figcaption, figure,
-  footer, header, hgroup, menu, nav, section {
-    display: block;
-  }
-  body {
-    line-height: 1;
-  }
-  ol, ul {
-    list-style: none;
-  }
-  blockquote, q {
-    quotes: none;
-  }
-  blockquote:before, blockquote:after,
-  q:before, q:after {
-    content: '';
-    content: none;
-  }
-  table {
-    border-collapse: collapse;
-    border-spacing: 0;
-  }
-  html, body, #app {
-    height: 100%;
-  }
-`
 const LeftColumn = styled.div`
   flex: 1.8;
   box-sizing: border-box;
@@ -120,7 +72,7 @@ class App extends React.Component {
   }
 
   renderNavBar() {
-    return this.state.scrollPos > 1000 ? <NavBar /> : <></>
+    return this.state.scrollPos > 500 ? <NavBar /> : <></>
   }
 
   calculateAvailable(month, day, availability) {
@@ -146,7 +98,8 @@ class App extends React.Component {
   }
 
   getData(id) {
-    axios.get(`http://localhost:3010/api/checkout/${id}`).then(({ data }) => {
+    axios.get(`/api/checkout/${id}`).then(({ data }) => {
+      console.log(data);
       let { availability } = data;
       delete data.availability;
       this.setState({
@@ -191,9 +144,7 @@ class App extends React.Component {
     return (
       <RowContainer className='rowContainer'>
         {this.renderNavBar()}
-        <TopBottomDummy />
         <Container>
-          <GlobalStyle />
           <LeftColumn />
           <RightColumn>
             <CheckoutBox
@@ -210,7 +161,6 @@ class App extends React.Component {
             />
           </RightColumn>
         </Container>
-        <TopBottomDummy />
       </RowContainer>
 
     );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const cors = require('cors');
+
 const { get } = require('./controller/checkoutController.js');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
