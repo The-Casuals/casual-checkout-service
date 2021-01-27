a4dc0e06e4ffbe2d4f4480198054fbd86d7ee89e
Remy Orans <remy.orans@gmail.com>
2021-01-26 21:28:17 -0800

    Map 80 to 3010 add bundle route

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
-app.use('/:id', express.static(path.join(__dirname, '..', 'public', 'bundle.js')));
+app.use('/:id', express.static(path.join(__dirname, '..', 'public')));
+app.use('/bundle', express.static(path.join(__dirname, '..', 'public', 'bundle.js')));
 app.get('/api/checkout/:id', get);
 
 const PORT = 3010;
